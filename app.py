1237098215b775bdebc8863cf14da22d67767ec0
Sailorrrrr <bgl9292@naver.com>
2022-04-15 16:13:30 +0900

    홈페이지 mnist작업

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/mnist', methods=['GET', 'POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnistform.html')
+    else:
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__)+'/upload/'+f.filename
+        f.save(path)
 
 if __name__ == '__main__':
     app.run(debug=True)
